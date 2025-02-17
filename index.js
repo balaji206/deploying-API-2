6551e3329ec2db65a8ca81c10ce80a5eeb4ad01e
balaji <balajier2006@gmail.com>
2025-02-17 17:12:16 +0530

    done

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { resolve } = require('path');
+require('dotenv').config();
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+
+const isAdmin = process.env.IS_ADMIN;
+
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}
+
+app.get('/', (req, res) => {
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
